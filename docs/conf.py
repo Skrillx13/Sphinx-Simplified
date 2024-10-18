75ccdbb4047db3a543f8c953832987b4aadcb376
Jonathan Fong <skrillx1300@gmail.com>
2024-10-18 14:30:15 +0800

    Added initial Documentation structure

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+# -- Libary Import --
+from datetime import datetime
+
+# -- Project Information --
+project = 'Sphinx Simplified'
+copyright = f"{datetime.now().year}, A Srillx Project"
+author = 'Skrillx'
+release = '1.0'
+
+# -- General Configuration --
+extensions = ['myst_parser']
+templates_path = ['templates']
+html_static_path = ['assets']
+exclude_patterns = ['build']
+html_show_sphinx = False
+
+# -- Theming --
+html_theme = 'sphinx13-theme'
+html_theme_path = ['..']
+html_css_files = [
+    'sphinx13-theme.css',
+]
